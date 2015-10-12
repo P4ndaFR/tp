7bf74094ee7f3da17f1a7f6e9f2d945742a56e66
SwagPanda <antoine.blondeau@isen-bretagne.fr>
2015-10-12 09:08:45 +0200

    Ajout des fichiers Create Table et Drop Table (DB)

--- tpMySQL/CTtp1.sql
+++ tpMySQL/CTtp1.sql
@@ -10,34 +10,43 @@ create table COMMANDE
 (
 	numcde 		INTEGER 		PRIMARY KEY,
 	datecde 	DATE,
-	numclient	INTEGER,
-	FOREIGN KEY	numclient 		REFERENCES CLIENT(numclient),		
+	numclient	INTEGER
+	
 )engine=innodb;
 create table PRODUIT
 (
-	reference 	INTEGER 		PRIMARY KEY,
+	reference 	VARCHAR(25) 		PRIMARY KEY,
 	design 		VARCHAR(100),
 	qtte_stock	INTEGER,
-	prix 		NUMERIC(5,5),
+	prix 		NUMERIC(5,5)
 )engine=innodb;
 create table COMMANDE_PRODUIT
 (
-	FOREIGN KEY	numcde 			REFERENCES COMMANDE (numcde),
-	FOREIGN KEY	reference 		REFERENCES PRODUIT(reference),
-	PRIMARY KEY (numcde,reference),
-	qtte_cde	INTEGER
+	qtte_cde	INTEGER,
+	numcde 		INTEGER,
+	reference 	VARCHAR(25),
+	PRIMARY KEY (numcde,reference)
 )engine=innodb;
 create table FACTURE
 (
 	numfact 	INTEGER			PRIMARY KEY,
 	datefact	DATE,
 	mode_regl	VARCHAR(6),
-	FOREIGN KEY numcde			REFERENCES COMMANDE,
+	numcde 		INTEGER
 )engine=innodb;
-create table FACTUE_PRODUIT
+create table FACTURE_PRODUIT
 (
-	FOREIGN KEY numfact			REFERENCES FACTURE,
-	FOREIGN KEY reference 		REFERENCES PRODUIT,
-	PRIMARY KEY (numfact,reference),
 	qtte_fact	INTEGER,
+	numfact 	INTEGER,
+	reference 	VARCHAR(25),
+	PRIMARY KEY (numfact,reference)
 )engine=innodb;
+
+alter table COMMANDE add FOREIGN KEY(numclient) REFERENCES CLIENT(numclient);
+alter table COMMANDE_PRODUIT 
+	add FOREIGN KEY	(numcde) 	REFERENCES COMMANDE (numcde),
+	add FOREIGN KEY	(reference) REFERENCES PRODUIT(reference);
+alter table FACTURE add FOREIGN KEY (numcde)	REFERENCES COMMANDE (numcde);
+alter table FACTURE_PRODUIT
+	add FOREIGN KEY (numfact)			REFERENCES FACTURE (numfact),
+	add FOREIGN KEY (reference) 		REFERENCES PRODUIT (reference);
